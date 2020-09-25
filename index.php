7f0e514cd88f1cdf13065fb25e2a220f4162be7a
Rajeiha <rajeiha122333@gmail.com>
2020-09-25 14:41:49 +0530

    HeadFinish

---
+++ index.php
@@ -0,0 +1,224 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shopping Shop</title>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Tangerine">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/bootstrap-grid.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap-grid.css">
+</head>
+<body>
+<div class="headBackGround" >
+  <div class="BlackShadow">  
+  <table>
+  <tr>
+  <td style="width:10%;">
+    
+  </td>
+
+  <td  style="width:50%;">
+    <ul class="nav justify-content-start">
+        <li class="nav-item">
+          <a class="nav-link" href="#"><img src="images/Tele.jpg" alt ="Insta" style="height:25px; width:25px; color:red;">
+          
+          Hotline : 1234567890   
+        </a>
+        </li>
+    </ul>
+
+  </td>
+  <td >
+    <ul class="nav justify-content-end">
+        <li class="nav-item">
+          <a class="nav-link" href="#"><img src="images/cart.jpg" alt ="Insta" style="height:25px; width:25px; color:red;">
+          
+          Cart
+          
+        </a>
+        </li>
+        <li class="nav-item">
+          <div class="dropdown">
+              <div><button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <img src="images/MyAccount.png" alt="Img" style="width:20px; height:20px;"> 
+              My Account 
+              </button></div>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+          </div>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"><img src="images/social media2.jpg" alt ="Insta" style="height:25px; width:25px;"></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"><img src="images/social media3.jpg" alt ="Face book" style="height:25px; width:25px;"></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"><img src="images/social media4.jpg" alt ="Google" style="height:25px; width:25px;"></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"><img src="images/social media5.jpg" alt ="Twitter" style="height:25px; width:25px;"></a>
+        </li>
+        
+    </ul>
+  </td>
+  <td style="width:5%;"> </td>
+  </tr>
+  </table> 
+  </div>
+<!-- End of First Part-->
+  <div class="BlackShadow">  
+  <table>
+  <tr>
+  <td style="width:10%;">
+    
+  </td>
+
+  <td  style="width:10%;">
+    <img src="images/Food_logo.jpg" alt="Food Logo" style="width:150px; height:50px;">
+  </td>
+  <td >
+    <ul class="nav justify-content-end">
+      <li class="nav-item">
+        <div class="dropdown">
+          <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            HOME
+          </button>
+        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">ABOUT US</a>
+      </li>
+      <li class="nav-item">
+        <div class="dropdown">
+            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              OUR MENU
+            </button>
+          <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <a class="dropdown-item" href="#">Action</a>
+            <a class="dropdown-item" href="#">Another action</a>
+            <a class="dropdown-item" href="#">Something else here</a>
+          </div>
+        </div>
+      </li>
+      <li class="nav-item">
+        <div class="dropdown">
+            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              PAGES
+            </button>
+          <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <a class="dropdown-item" href="#">Action</a>
+            <a class="dropdown-item" href="#">Another action</a>
+            <a class="dropdown-item" href="#">Something else here</a>
+          </div>
+        </div>
+      </li>
+      <li class="nav-item">
+        <div class="dropdown">
+            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              OUR BLOG
+            </button>
+          <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <a class="dropdown-item" href="#">Action</a>
+            <a class="dropdown-item" href="#">Another action</a>
+            <a class="dropdown-item" href="#">Something else here</a>
+          </div>
+        </div>
+      </li>
+      <li class="nav-item">
+        <div class="dropdown">
+            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              CONTACT US
+            </buttossn>
+          <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <a class="dropdown-item" href="#">Action</a>
+            <a class="dropdown-item" href="#">Another action</a>
+            <a class="dropdown-item" href="#">Something else here</a>
+          </div>
+        </div>
+      </li>
+      
+      <li class="nav-item">
+        <button type="button" class="ButtonColor">BOOK YOUR TABLE</button>
+      </li>
+    </ul>  
+  </td>
+  <td style="width:10%;"> </td>
+  </tr>
+  </table> 
+  </div>
+  <!-- End of Second Part -->
+  <div class="ThirdPart">  
+    <button type="button" class="BlackShadow">
+      <p class="w3-tangerine">Shopping Cart </p>  
+      <p style="color:white;">HOME > SHOPPING CART</p>
+      
+    </button> 
+  </div>  
+  <!-- End of Third Part-->
+
+</div>
+<!-- End of the Head part-->
+<div>
+<table class="center">
+<tr>
+<td>  
+<p  >First Part</p>
+<p >First Part</p>
+</td>
+</tr>
+<tr>
+<td>
+<table class="table" id="FoodList">
+  <thead class="thead-light">
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</td>
+</tr>
+</table>
+</div>
+<!-- End of the Content part-->
+<div>
+
+</div>
+<!-- End of the Foot part-->
+
+</body>
+</html>
